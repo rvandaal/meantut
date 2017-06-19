4788a132ba7bfd826db21351d6c7cd92ff2b6552
Rob van Daal <rvandaal@hotmail.com>
2017-06-19 20:22:28 +0200

    Added mongoose and mongodb

---
+++ config/database.js
@@ -0,0 +1,7 @@
+var crypto = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+	uri: 'mongodb://localhost:27017/' + this.db,
+	secret: crypto,
+	db: 'meantut-angular-2'
+}
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+	if(err) {
+		console.log('Could NOT connect to database: ' + err);
+	} else {
+		console.log('Connected to database: ' + config.db);
+	}
+});
 
 app.get('*', (req, res) => {
 	res.send('Hello world!');
